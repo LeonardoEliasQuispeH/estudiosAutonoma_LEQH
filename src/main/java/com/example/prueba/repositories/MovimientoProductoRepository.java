67bc9bc094973bb65fe43fd343a109ea96e50fd6
LeonardoEliasQuispeH <147902081+LeonardoEliasQuispeH@users.noreply.github.com>
2025-07-13 01:11:34 -0500

    Update MovimientoProductoRepository.java

--- src/main/java/com/example/prueba/repositories/MovimientoProductoRepository.java
+++ src/main/java/com/example/prueba/repositories/MovimientoProductoRepository.java
@@ -1,11 +1,7 @@
 package com.example.prueba.repositories;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.prueba.entities.MovimientoProducto;
-
 public interface MovimientoProductoRepository extends JpaRepository<MovimientoProducto, Long>{
     
     List<MovimientoProducto> findTop30ByOrderByFechaMovimientoDesc();
